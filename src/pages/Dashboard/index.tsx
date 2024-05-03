f26d639388d3a9c7cc914621d080d0ea4ccfb39f
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2024-05-03 18:06:44 +0200

    feat: welcome page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,15 +7,18 @@ import { RootState } from "../../store";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import Notification from "../../components/Notification";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import TopUsers from "../../features/dashboard/TopUsers";
 import ProStatistics from "../../features/dashboard/Professor/Statistics";
 import StudStatistics from "../../features/dashboard/Student/Statistics";
 import StudActivity from "../../features/dashboard/Student/Activity";
 import ProActivity from "../../features/dashboard/Professor/Activity";
+import { useNavigate } from "react-router-dom";
+import { QuizIcon } from "../../assets/icons";
 
 const DashboardPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const user = useSelector((state: RootState) => state.auth.user);
 
@@ -65,15 +68,34 @@ const DashboardPage = () => {
 
   useEffect(() => {
     checkNotifications();
+    console.log("user.role: ", user.role)
   }, [notification]);
 
   return (
     <Box className="dashboard__root">
-      <Box className="dashboard__container">
-        <TopUsers />
-        {/* @ts-ignore */}
-        {roleBasedElements.statistics[user.role]}
-      </Box>
+      { user.role === "" ?
+        <Box>
+          <TopUsers />
+          
+          <Box className="login__dashboard-link">
+            <Typography className="login__dashboard-header">
+              To see more statistics, please, log in 
+              <a className="login__dashboard-a" onClick={() => navigate('/login')}>here</a>
+            </Typography>
+          </Box>
+
+          <Box className="login__dashboard-main-icon">
+            <QuizIcon className="dashboard__icon"/>
+          </Box>
+        </Box>
+        : 
+        <Box className="dashboard__container">
+          <TopUsers />
+          {/* @ts-ignore */}
+          {roleBasedElements.statistics[user.role]}
+        </Box>
+      }
+
       {/* @ts-ignore */}
       {roleBasedElements.activity[user.role]}
     </Box>
